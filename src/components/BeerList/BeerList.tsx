6901237f4b97524f19548981afbbb0da9dff3ff7
DI3BOSS <diegofbermudez@gmail.com>
2023-02-10 17:40:07 +0100

    Add Styles to BeerList component

--- src/components/BeerList/BeerList.tsx
+++ src/components/BeerList/BeerList.tsx
@@ -1,18 +1,20 @@
 import { BeersStructure } from "../../data/types";
 import BeerCard from "../BeerCard/BeerCard";
+import { BeerListStyled } from "./BeerListStyed";
+
 interface BeerListProps {
   beers: BeersStructure;
 }
 
 const BeerList = ({ beers }: BeerListProps): JSX.Element => {
   return (
-    <>
+    <BeerListStyled>
       {beers.map((beer) => (
         <li key={beer.id}>
           <BeerCard beer={beer} />
         </li>
       ))}
-    </>
+    </BeerListStyled>
   );
 };
 export default BeerList;
