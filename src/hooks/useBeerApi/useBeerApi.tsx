c959acb946b2e93b7d067bdefc21e053d6c403f8
Carles Galan <galan.carles@gmail.com>
2023-02-10 19:10:27 +0100

    Modify custom hook with set and unset loading before and after fetch

--- src/hooks/useBeerApi/useBeerApi.tsx
+++ src/hooks/useBeerApi/useBeerApi.tsx
@@ -3,6 +3,11 @@ import { useContext, useCallback } from "react";
 
 import BeersContext from "../../store/contexts/beers/BeersContext";
 import { loadBeersActionCreator } from "../../store/actions/beers/beersActionCreators";
+import UiContext from "../../store/contexts/ui/UiContext";
+import {
+  setIsLoadingActionCreator,
+  unsetIsLoadingActionCreator,
+} from "../../store/actions/ui/uiActionCreators";
 
 export const currentPage = 1;
 export const beersPerPage = 1;
@@ -10,16 +15,18 @@ export const beerApiUrl = "https://api.punkapi.com/v2/beers?";
 
 const useBeerApi = () => {
   const { dispatch } = useContext(BeersContext);
+  const { dispatch: uiDispatch } = useContext(UiContext);
 
   const getBeersFromApi = useCallback(async () => {
+    uiDispatch(setIsLoadingActionCreator());
     const responseFromBeerApi = await fetch(
       `${beerApiUrl}page=${currentPage}&per_page=${beersPerPage}`
     );
     const beerListInformation =
       (await responseFromBeerApi.json()) as BeersStructure;
-
+    uiDispatch(unsetIsLoadingActionCreator());
     dispatch(loadBeersActionCreator(beerListInformation));
-  }, [dispatch]);
+  }, [dispatch, uiDispatch]);
 
   return { getBeersFromApi };
 };
