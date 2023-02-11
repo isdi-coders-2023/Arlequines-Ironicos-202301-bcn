e8ea01ac5a84d800a14c8f83354e0239d2abe8d0
Cristina <cristinajmartin98@gmail.com>
2023-02-11 21:16:56 +0100

    Add filters component and its styles

---
+++ src/components/Filters/Filters.tsx
@@ -0,0 +1,13 @@
+import FiltersStyled from "./FiltersStyled";
+
+const Filters = () => {
+  return (
+    <FiltersStyled className="filters">
+      <button>Classics</button>
+      <button>Strongest</button>
+      <button>Most Bitter</button>
+    </FiltersStyled>
+  );
+};
+
+export default Filters;
