00ec2d5cafa23256553f074c285c6c8fb5238865
Carles Galan <galan.carles@gmail.com>
2023-02-12 17:37:29 +0100

    Add ui dispatcher before and after fetch into the hook

--- src/hooks/useBeerApi/useBeerApi.tsx
+++ src/hooks/useBeerApi/useBeerApi.tsx
@@ -4,6 +4,12 @@ import { useContext, useCallback } from "react";
 import BeersContext from "../../store/contexts/beers/BeersContext";
 import { loadBeersActionCreator } from "../../store/actions/beers/beersActionCreators";
 import convertKebabCaseToCamelCase from "../../utils/convertKebabCaseToCamelCase/convertKebabCaseToCamelCase";
+import {
+  setIsLoadingActionCreator,
+  unsetIsLoadingActionCreator,
+} from "../../store/actions/ui/uiActionCreators";
+
+import UiContext from "../../store/contexts/ui/UiContext";
 
 export const currentPage = 1;
 export const beersPerPage = 48;
@@ -11,20 +17,23 @@ export const beerApiUrl = "https://api.punkapi.com/v2/beers?";
 
 const useBeerApi = () => {
   const { dispatch } = useContext(BeersContext);
+  const { dispatch: uiDispatch } = useContext(UiContext);
 
   const getBeersFromApi = useCallback(async () => {
     try {
+      uiDispatch(setIsLoadingActionCreator());
       const responseFromBeerApi = await fetch(
         `${beerApiUrl}page=${currentPage}&per_page=${beersPerPage}`
       );
       const beerListInformation = convertKebabCaseToCamelCase(
         await responseFromBeerApi.json()
       ) as CamelCaseBeersStructure;
+      uiDispatch(unsetIsLoadingActionCreator());
       dispatch(loadBeersActionCreator(beerListInformation));
     } catch (error) {
       return (error as Error).message;
     }
-  }, [dispatch]);
+  }, [dispatch, uiDispatch]);
 
   return { getBeersFromApi };
 };
